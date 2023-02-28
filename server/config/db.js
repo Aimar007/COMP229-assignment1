aedef028fc8e03a773ce049781c3ee9a06061d26
Huang, Guo <107522629+Aimar007@users.noreply.github.com>
2023-02-27 23:36:51 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
-    "URI":"mongodb+srv://guo:huang@cluster0.qvgy75h.mongodb.net/?retryWrites=true&w=majority"
-    //"URI":"mongodb://localhost/contact_list"
+    //"URI":"mongodb+srv://guo:huang@cluster0.qvgy75h.mongodb.net/?retryWrites=true&w=majority"
+    "URI":"mongodb://localhost/contact_list"
     // "URI":"mongodb://127.0.0.1:27017/contact_list"
 }
